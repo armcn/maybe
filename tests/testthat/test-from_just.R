957a7eddf18c5e676b8a4faadb6c510fa72a30d1
Andrew McNeil <andrew.richard.mcneil@gmail.com>
2022-04-02 08:54:21 -0700

    Add tests for from_just

--- tests/testthat/test-from_just.R
+++ tests/testthat/test-from_just.R
@@ -1,20 +1,20 @@
-test_that("from_maybe will fail with non-maybe values", {
+test_that("from_just will fail with non-maybe values", {
   for_all(
     a = anything(),
-    property = function(a) from_maybe(a) %>% expect_error()
+    property = function(a) from_just(a) %>% expect_error()
   )
 })
 
-test_that("from_maybe will fail with nothing", {
-   nothing() %>% from_maybe() %>% expect_error()
+test_that("from_just will fail with nothing", {
+   nothing() %>% from_just() %>% expect_error()
 })
 
-test_that("from_maybe unwraps a just value", {
+test_that("from_just unwraps a just value", {
   for_all(
     a = anything(),
     property = function(a)
       just(a) %>%
-        from_maybe() %>%
+        from_just() %>%
         expect_identical(a)
   )
 })
